ae9fd32394c13452505cb8dba9abffa765e9347f
someonehappy <someonehappyau@gmail.com>
2020-02-15 11:39:39 +1100

    1. add debug for msuser

--- msuser/index.js
+++ msuser/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.all('/', function (req, res, next) {
+    console.log(req);
+}
 app.get('/', (req, res) => res.send('MS User is running.'))
 app.get('/user/1', (req, res) => res.json({uid: 1, uname: 'john', pwd: 'Test1234' }) )
 
