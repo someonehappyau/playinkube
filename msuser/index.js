4d2e93dfac29b0fcc074279f0814342b5971cf40
someonehappy <someonehappyau@gmail.com>
2020-02-15 14:25:04 +1100

    1. change routine

--- msuser/index.js
+++ msuser/index.js
@@ -2,11 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.all('/', function (req, res, next) {
-    console.log(req);
-    next();
-})
+
 app.get('/', (req, res) => res.send('MS User is running.'))
-app.get('/user/1', (req, res) => res.json({uid: 1, uname: 'john', pwd: 'Test1234' }) )
+app.get('/get/1', (req, res) => res.json({uid: 1, uname: 'john', pwd: 'Test1234' }) )
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
