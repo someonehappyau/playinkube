fd1b335700a7dcf5cea48d1d626323f5db583156
someonehappy <someonehappyau@gmail.com>
2020-02-17 00:03:45 +1100

    1. change msuser

--- msuser/index.js
+++ msuser/index.js
@@ -6,11 +6,12 @@ const port = 3000
 app.get('/', (req, res) => res.send('MS User is running.'))
 app.get('/get/1', (req, res) => res.json({uid: 1, uname: 'john', pwd: 'Test1234' }) )
 app.get('/login', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
     if (req.query.username==='john' && req.query.password==='Test1234'){
         res.json({id: '1', username: 'john', nickname: 'John The Champion'})
     }
     else{
-        res.json({});
+        res.json({nickname:'Not Found'});
     }
 })
 
